ed47e07ae76efe173cb72c4f92a40534f4be71dd
DimMindfling <dimendroider@gmail.com>
2021-03-03 01:52:37 +0300

    Lesson18 setInterval ERROR

--- script/script.js
+++ script/script.js
@@ -1,36 +1,31 @@
-window.addEventListener ('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function () {
   'use strict';
-  console.log ('hello');
+  console.log('hello');
 
   //Timer
-  function countTimer (deadline) {
+  function countTimer(deadline) {
     //получаем элементы один раз
-    let timerHours = document.querySelector ('#timer-hours'),
-      timerMinuts = document.querySelector ('#timer-minutes'),
-      timerSeconds = document.querySelector ('#timer-seconds');
+    let timerHours = document.querySelector('#timer-hours'),
+      timerMinuts = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
 
     // сюда всё что вычисляется
-    function getTimeRemaining () {
-      let dateStop = new Date (deadline).getTime (); //будущ дата
-      let dateNow = new Date ().getTime (); //текущ дата
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(); //будущ дата
+      let dateNow = new Date().getTime(); //текущ дата
       let timeRemaining = (dateStop - dateNow) / 1000; //ms/1000 = sec
 
-      let seconds = Math.floor (timeRemaining % 60); // sec
-      let minutes = Math.floor (timeRemaining / 60 % 60); //min
-      let hours = Math.floor (timeRemaining / 60 / 60 % 24); // hours
+      let seconds = Math.floor(timeRemaining % 60); // sec
+      let minutes = Math.floor((timeRemaining / 60) % 60); //min
+      let hours = Math.floor((timeRemaining / 60 / 60) % 24); // hours
 
       return {timeRemaining, hours, minutes, seconds};
     }
 
-    // console.log('seconds: ', seconds);
-    // console.log('minutes: ', minutes);
-    // console.log('hours: ', hours);
-    // console.log('days: ', days);
-    // console.log('timeRemaining: ', timeRemaining);
-
-    function updateClock () {
-      let timer = getTimeRemaining ();
-      console.log ('timer: ', timer);
+    //обновление таймера
+    function updateClock() {
+      let timer = getTimeRemaining();
+      console.log('timer: ', timer);
 
       timerHours.textContent = timer.hours;
       timerMinuts.textContent = timer.minutes;
@@ -38,14 +33,16 @@ window.addEventListener ('DOMContentLoaded', function () {
 
       if (timer.timeRemaining > 0) {
         // * рекурсивно вызываем через 1с
-        setTimeout (updateClock, 1000);
+        // setTimeout(updateClock, 1000);
+        setInterval(updateClock, 1000); // !!!! рекурсивно вызывать можно только setTimeout()
       }
     }
-    // * запускаем ОДИН рза
+    
+    // * запускаем ОДИН раз
     // todo переделать через setInterval()
-    updateClock ();
+    updateClock();
   }
 
   //тест таймера
-  countTimer ('04/01/2021');
+  countTimer('04/01/2021');
 });
