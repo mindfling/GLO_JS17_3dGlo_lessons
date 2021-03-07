2771236025dbf9334eeb90c32eabbc2862c2457b
DimMindfling <dimendroider@gmail.com>
2021-03-08 01:20:20 +0300

    Lesson18 feat ES6

--- script/script.js
+++ script/script.js
@@ -1,18 +1,20 @@
 // запуск после загрузки страници тоже что и defer
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
   'use strict';
   console.log('hello\nthis is 3dGLO');
 
 
-  //Timer
-  function countTimer(deadline) {
-    //получаем элементы один раз
+  //! Timer ДЗ 18
+  //* функция получает строку времени в формате mm/dd/yyyy
+  //* анимирует таймер сколько времени осталось до этой даты
+  const countTimer = deadline => {
+    //получаем элементы один раз поля таймера
     let timerHours = document.querySelector('#timer-hours'),
       timerMinuts = document.querySelector('#timer-minutes'),
       timerSeconds = document.querySelector('#timer-seconds');
 
-    // сюда всё что вычисляется
-    function getTimeRemaining() {
+    //! сюда всё что вычисляется
+    const getTimeRemaining = () => {
       let dateStop = new Date(deadline).getTime(); //будущ дата
       let dateNow = new Date().getTime(); //текущ дата
       let timeRemaining = (dateStop - dateNow) / 1000; //ms/1000 = sec
@@ -22,10 +24,10 @@ window.addEventListener('DOMContentLoaded', function () {
       let hours = Math.floor((timeRemaining / 60 / 60) % 24); // hours
 
       return {timeRemaining, hours, minutes, seconds};
-    }
+    };
 
     //обновление таймера
-    function updateClock() {
+    const updateClock = () => {
       let timer = getTimeRemaining();
       console.log('timer: ', timer);
 
@@ -35,17 +37,17 @@ window.addEventListener('DOMContentLoaded', function () {
         timerSeconds.textContent = (timer.seconds < 10 ? '0' : '') + timer.seconds;
       } else {
         // * напоминалка если дата уже прошла то обратный таймер уже по нулям
-        timerHours.textContent = 0;
-        timerMinuts.textContent = 0;
-        timerSeconds.textContent = 0;
+        timerHours.textContent = '00';
+        timerMinuts.textContent = '00';
+        timerSeconds.textContent = '00';
       }
-    }
+    };
 
     // * запускаем ОДИН раз, один раз планируем вызов
     // * переделываем через setInterval() по сути тот же setTimeout() с рекурсией
-    setInterval(updateClock, 1000);
-  }
+    setInterval(updateClock, 1000); // 1 sec
+  };
 
-  //тест таймера
+  //тест таймера сколько часов остается до дня программиста
   countTimer('9 13 2021');
 });
